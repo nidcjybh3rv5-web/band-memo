fefa1b02f43295aa562e355d4ccac69423a15a11
Apple_14 <nidcjybh3rv5@gmail.com>
2026-08-06 18:48:48 +0800

    Fix escapeHtml test for quote escaping

--- tests/render.test.js
+++ tests/render.test.js
@@ -3,5 +3,5 @@ const { escapeHtml } = require('../src/security');
 
 assert.strictEqual(escapeHtml('<script>'), '&lt;script&gt;');
 assert.strictEqual(escapeHtml('a & b'), 'a &amp; b');
-assert.strictEqual(escapeHtml('"quote"'), '&quot;quote&quot;');
+assert.strictEqual(escapeHtml('\"quote\"'), '&quot;quote&quot;');
 console.log('render/escape tests passed');
